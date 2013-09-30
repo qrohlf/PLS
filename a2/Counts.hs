49619b42911f2966ad7673651ea411c6b9b01abf
Quinn Rohlf <qr@qrohlf.com>
2013-09-29 17:16:11 -0700

    Add type sig to counts

--- a2/Counts.hs
+++ a2/Counts.hs
@@ -6,6 +6,7 @@ main = do
     contents <- getContents
     print (take 10 (sortBy tupleSort (wordCount contents)))
 
+wordCount :: String -> [(String, Int)]
 wordCount a = let wordlist = group ( sort (words a) ) in (zip (map (head) wordlist) (map (length) wordlist))
 
 tupleSort (a1, a2) (b1, b2) 
