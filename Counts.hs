c33f737eb2e252ca3ccb5d4feed7a2e8efed9225
Quinn Rohlf <qr@qrohlf.com>
2013-09-29 17:06:55 -0700

    Assignment 2 (mostly) ready'

---
+++ Counts.hs
@@ -0,0 +1,13 @@
+import System.IO 
+import Data.List
+-- import "a1.hs"
+
+main = do 
+	contents <- getContents
+	print (take 10 (sortBy tupleSort (wordCount contents)))
+
+wordCount a = let wordlist = group ( sort (words a) ) in (zip (map (head) wordlist) (map (length) wordlist))
+
+tupleSort (a1, a2) (b1, b2) 
+	| b2 < a2 = LT
+	| otherwise = GT
