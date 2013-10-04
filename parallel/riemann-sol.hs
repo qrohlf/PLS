fdcc6aa4b5e5a7a2c8a2e8cd1fa3c8406a91f9d5
Miles Crabill <miles@milescrabill.com>
2013-10-04 10:45:46 -0700

    Added parallel riemann sum solution

--- parallel/riemann-sol.hs
+++ parallel/riemann-sol.hs
@@ -8,4 +8,6 @@ circle x = sqrt (abs (1 - x^2))
 area x1 x2 = (x2 - x1) * circle x2
 
 estimate (x:y:ys) = (area x y) + estimate (y:ys)
-estimate [x] = 0
+estimate _ = 0
+
+pi' n = 2 * estimate [(-1), (-1 + (2/n)) .. 1]
