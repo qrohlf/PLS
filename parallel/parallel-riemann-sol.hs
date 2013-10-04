c58c5529bd102c811463066fe5df322116547806
Miles Crabill <miles@milescrabill.com>
2013-10-04 11:06:24 -0700

    added solution for parallel riemann

---
+++ parallel/parallel-riemann-sol.hs
@@ -0,0 +1,21 @@
+--sequential version
+import Control.Parallel
+import Control.Parallel.Strategies
+
+--Circle
+circle x = sqrt (abs (1 - x^2))
+
+--Area of a Riemann Rectangle
+area x1 x2 = (x2 - x1) * circle x2
+
+estimate (x:y:ys) = (fencepost `par` fence) `pseq` (fencepost + fence)
+    where fencepost = area x y
+          fence = estimate (y:ys)
+estimate _ = 0
+
+pi' n = 2 * estimate [(-1), (-1 + (2/n)) .. 1]
+
+main = do
+    putStrLn "How many slices?"
+    n <- getLine
+    putStrLn $ show $ pi' $ (read n)
