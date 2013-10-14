62c62d30c9c46dd4dac504e645d1f97c73e07557
Quinn Rohlf <qr@qrohlf.com>
2013-10-14 12:09:44 -0700

    Fixed counts formatting'

--- a2/Counts.hs
+++ a2/Counts.hs
@@ -6,7 +6,7 @@ import Data.List
 main = do 
     contents <- getContents
     args <- getArgs 
-    print (take (read (head args)) (sortBy tupleSort (wordCount contents)))
+    putStr (output (take (read (head args)) (sortBy tupleSort (wordCount contents))))
 
 wordCount :: String -> [(String, Int)]
 wordCount a = let wordlist = group ( sort (words a) ) in (zip (map (head) wordlist) (map (length) wordlist))
@@ -14,3 +14,6 @@ wordCount a = let wordlist = group ( sort (words a) ) in (zip (map (head) wordli
 tupleSort (a1, a2) (b1, b2) 
     | b2 < a2 = LT
     | otherwise = GT
+
+output (x:xs) = fst x ++ ":" ++ show (snd x) ++ "\n" ++ output xs
+output [] = ""
