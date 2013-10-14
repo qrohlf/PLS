0991afd87468d9b4d7cde33aede7c5710c8e9a7f
Quinn Rohlf <qr@qrohlf.com>
2013-10-14 12:02:51 -0700

    add type sigs to Pairs.hs and rename module from Pair to Pairs

--- a2/Pairs.hs
+++ a2/Pairs.hs
@@ -1,5 +1,7 @@
-module Pair (pairMap, withBoth) where
+module Pairs (pairMap, withBoth) where
 
+    pairMap :: (t -> t1) -> (t, t) -> (t1, t1)
     pairMap f (a, b) = (f a, f b) 
 
+    withBoth :: (t1 -> t2 -> t) -> (t1, t2) -> t
     withBoth f (a, b) = f a b 
